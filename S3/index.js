8c1bc8943f24f40ba9f5356de1e80aa2e5d9f484
Liangpin_Hou <bz20100405@163.com>
2017-12-08 16:33:56 +0800

    changed S3 to use 'for'
    in fact, we don't need $(this).text(), we just need to change url every time.

--- S3/index.js
+++ S3/index.js
@@ -1,37 +1,34 @@
 var alive = [0, 1, 2, 3, 4];
 $(function(){
-  // $(".apb").bind("click", robot);
-  $(".apb").bind("click",function() {
-    $("#control-ring li").click();
-  });
-  $("#control-ring li").bind("click", function() {
-    (function(index) {
-      if(index.length != 1)
-        return;
-      index = index.charCodeAt()-65;
-      getSmallRed(index);
-      var myself = $("#control-ring li:eq("+index.toString()+")");
-      var obj = $("#control-ring span:eq("+myself.index().toString()+")");      
-      $.get(index+1, function(result) {
-        console.log(index);
-        obj.text(result);
-        myself.removeClass("untouch").addClass("touch");
-        alive.splice(alive.indexOf(myself.index()), 1);  //kill one
-        if(alive.length == 0)
-          count();        
-      });
-    })($(this).text()[0])
-  });
+  $(".apb").bind("click", robot);
   // TO DO 
   // block response
   // $("#button").bind("mouseleave", reset);  
 });
+function robot() {
+  for(var i = 0; i < 5; i++) {
+    getSmallRed(i);
+    var myself = $("#control-ring li:eq("+i.toString()+")");
+    var obj = $("#control-ring span:eq("+myself.index().toString()+")");
+    f1(obj, myself, i);
+  }  
+}
 
 function getSmallRed(index) {
   var myself = $("#control-ring li:eq("+index.toString()+")");
   myself.removeClass("touch").addClass("untouch");
   var obj = $("#control-ring span:eq("+myself.index().toString()+")");
   obj.text("...").removeClass("hide").addClass("show");  
+}
+
+function f1(obj, myself, times) {
+  $.get("/"+times, function(result){
+    obj.text(result);
+    myself.removeClass("untouch").addClass("touch");
+    alive.splice(alive.indexOf(myself.index()), 1);  //kill one
+    if(alive.length == 0)
+      count();
+  });
 }
 
 function count() {
